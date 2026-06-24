9f2faebbbf4deec8e5252be547f8aebaceb6d7e8
Tair <gidrolaz.tr@gmail.com>
2026-06-24 21:52:51 +0700

    Оптимизация

--- algorithms/sort/bubble/bubble.c
+++ algorithms/sort/bubble/bubble.c
@@ -2,19 +2,24 @@
 
 int sort(int *array, int size)
 {
-    int tmp, n = size;
+    int tmp, swapped = 0;
 
-    for (int i = 0; i < n - 2; i++)
+    for (int i = 0; i < size - 1; i++)
     {
-        for (int j = n - 1; j >= i + 1; j--)
+        swapped = 0;
+        for (int j = size - 1; j >= i + 1; j--)
         {
             if (array[j] < array[j - 1])
             {
                 tmp = array[j];
                 array[j] = array[j - 1];
                 array[j - 1] = tmp;
+                swapped = 1;
             }
         }
+
+        if (swapped == 0)
+            return 1;
     }
 
     return 1;
