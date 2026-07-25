f297a00310a3e1b95c7f9f431fc35275be4fdef4
Tair <gidrolaz.tr@gmail.com>
2026-07-25 17:06:26 +0700

    Обновлен алгоритм подсчета. И добавлено описание сортировки

--- algorithms/sort/src/counter/counter.c
+++ algorithms/sort/src/counter/counter.c
@@ -1,24 +1,37 @@
 #include <stdlib.h>
 #include "sort.h"
 
-// Для простоты приниамется, что диапазон элементолв есть size (от 1 до size)
 int sort(int *array, int size)
 {
-    int *array_count = calloc(sizeof(int), size), ptr = 0;
+    int max_k = array[0], min_k = array[0];
 
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] > max_k)
+        {
+            max_k = array[i];
+        }
+        if (array[i] < min_k)
+        {
+            min_k = array[i];
+        }
+    }
+
+    int size_count = max_k - min_k + 1;
+    int *array_count = calloc(sizeof(int), size_count), ptr = 0;
     if (!array_count) 
         return -1;
 
     for (int i = 0; i < size; i++)
     {
-        array_count[array[i] - 1]++;
+        array_count[array[i] - min_k]++;
     }
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size_count; i++)
     {
         for (int j = 0; j < array_count[i]; j++)
         {
-            array[ptr] = i + 1;
+            array[ptr] = min_k + i;
             ptr++;
         }
     }
